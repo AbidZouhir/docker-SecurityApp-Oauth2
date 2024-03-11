11b491813a6b12e8bedcc69d94bf3709e46d94a0
zouhir <zouhir.etransit@gmail.com>
2024-03-11 15:38:27 +0000

    6 commit

--- customer-front-thymleaf/src/main/java/ma/mthymleaf/customerfrontthymleaf/web/CustomerController.java
+++ customer-front-thymleaf/src/main/java/ma/mthymleaf/customerfrontthymleaf/web/CustomerController.java
@@ -4,17 +4,23 @@ import ma.mthymleaf.customerfrontthymleaf.entities.Customer;
 import ma.mthymleaf.customerfrontthymleaf.repository.CustomerRepository;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @AllArgsConstructor
+@SuppressWarnings("unchecked")
 public class CustomerController {
     private CustomerRepository customerRepository;
+    private ClientRegistrationRepository clientRegistrationRepository;
 
     @GetMapping("/customers")
     @PreAuthorize("hasAuthority('ADMIN')")
@@ -44,4 +50,21 @@ public class CustomerController {
 
         return "notAuthorized";
     }
+    @GetMapping("/oauth2Login")
+    public String oauth2Login(Model model){
+        String authorizationRequestBaseUri = "oauth2/authorization";
+        Map<String, String> oauth2AuthenticationUrls = new HashMap();
+        Iterable<ClientRegistration> clientRegistrations = (Iterable<ClientRegistration>) clientRegistrationRepository;;
+        clientRegistrations.forEach(registration ->{
+            oauth2AuthenticationUrls.put(registration.getClientName(),
+                    authorizationRequestBaseUri + "/" + registration.getRegistrationId());
+        });
+
+        oauth2AuthenticationUrls.forEach(
+                (key,value)-> System.out.println(key + "||||" + value)
+        );
+        model.addAttribute("urls", oauth2AuthenticationUrls);
+        return "oauth2Login";
+    }
 }
+
